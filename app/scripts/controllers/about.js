4b21542f526608320c78c18d7f70eb68aa726391
florianguembel <florian.guembel@die-interaktiven.de>
2016-07-27 22:15:12 +0200

    * kickstarted yeoman angular app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bookingCalendarApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bookingCalendarApp
+ */
+angular.module('bookingCalendarApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bookingCalendarApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bookingCalendarApp
+ */
+angular.module('bookingCalendarApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
