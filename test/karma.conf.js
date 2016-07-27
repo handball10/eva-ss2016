f88a48ca4cc06f75f140deab7947daa8b273747e
florianguembel <florian.guembel@die-interaktiven.de>
2016-07-27 22:57:32 +0200

    *  added angular material * added angular firebase and firebase plugin * cleaned scss file * cleaned index.html

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -20,6 +20,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
@@ -27,6 +28,13 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/moment/moment.js',
+      'bower_components/fullcalendar/dist/fullcalendar.js',
+      'bower_components/fullcalendar-scheduler/dist/scheduler.js',
+      'bower_components/firebase/firebase.js',
+      'bower_components/angular-aria/angular-aria.js',
+      'bower_components/angular-messages/angular-messages.js',
+      'bower_components/angular-material/angular-material.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
