fbe72699fc419e833c063775ef4ee90f932ed09c
florianguembel <florian.guembel@die-interaktiven.de>
2016-08-13 13:02:32 +0200

    * login controller

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bookingCalendarApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the bookingCalendarApp
+ */
+angular.module('bookingCalendarApp')
+    .controller('LoginCtrl', function ($scope, AuthService) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    });
