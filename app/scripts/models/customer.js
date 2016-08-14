a4d82c8829e6b19609045600e32713175907496d
florianguembel <florian.guembel@die-interaktiven.de>
2016-08-14 13:38:48 +0200

    * added properties to customer

--- app/scripts/models/customer.js
+++ app/scripts/models/customer.js
@@ -8,16 +8,64 @@
  * Factory in the bookingCalendarApp.
  */
 angular.module('bookingCalendarApp')
-  .factory('Customer', function () {
-    // Service logic
-    // ...
+    .factory('Customer', function () {
+        function Customer(properties){
 
-    var meaningOfLife = 42;
+            var self = this;
 
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
-    };
-  });
+            //this.Id         = undefined;
+            this.Company    = undefined;
+            this.BirthDate  = undefined;
+            this.FirstName  = undefined;
+            this.LastName   = undefined;
+            this.Street   = undefined;
+            this.City   = undefined;
+            this.ZipCode   = undefined;
+            this.Email   = undefined;
+            this.Phone   = undefined;
+            this.Custom   = undefined;
+
+            /**
+             * @private
+             * @method getComputedProperties
+             * @param properties
+             */
+            function getComputedProperties(properties) {
+
+                // Convert all dateStrings to date Objects
+                properties = _.mapValues(properties, function (property, name) {
+                    if (!_.isFunction(property) && angular.isDefined(property) && name.indexOf('Date') >= 0) {
+                        var m = moment(property);
+                        return m.isValid() ? m.toDate() : property;
+                    }
+
+                    return property;
+                });
+
+
+                return properties;
+            }
+
+            /**
+             * @private
+             * @method extend
+             * @param properties
+             */
+            function extend(properties) {
+
+
+                if (!properties) {
+                    return;
+                }
+                angular.extend(self, properties);
+                angular.extend(self, getComputedProperties(properties));
+            }
+
+            this.extend = extend;
+
+            extend(properties);
+        }
+
+
+        return Customer;
+    });
